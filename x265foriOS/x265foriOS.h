c451aa37b82b2ac0135b1af9cd53d85f7142b64f
mihuaz2Z <mihuayouxi@163.com>
2017-05-27 11:21:35 +0800

    Initial Commit

---
+++ x265foriOS/x265foriOS.h
@@ -0,0 +1,13 @@
+//
+//  x265foriOS.h
+//  x265foriOS
+//
+//  Created by Ruiwen Feng on 2017/5/27.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface x265foriOS : NSObject
+
+@end
